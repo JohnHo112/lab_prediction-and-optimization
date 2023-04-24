9dca871d4cebc9c3fef4d53b545921598790bd0a
JohnHo112 <hokengseng0112@gmail.com>
2023-04-25 01:57:31 +0800

    add lab10

---
+++ lab10/NewtonMethod.py
@@ -0,0 +1,84 @@
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# a = np.array([2, -6, 2])  # y=2*(x-1.5)^2-2.5 [2, -6, 2]
+# f = np.poly1d(a)
+# l = 0.5
+# n = 1000
+# x = -5
+
+
+# def newton_method(x, f, l, n):
+#     dp = f.deriv()
+#     ddp = dp.deriv()
+
+#     for _ in range(n):
+#         x = x-l*dp(x)/ddp(x)
+
+#     return x
+
+
+# x = newton_method(x, f, l, n)
+# y = f(x)
+# print(x, y)
+
+
+# plt.plot(np.linspace(-30, 30, 61), f(np.linspace(-30, 30, 61)))
+# plt.show()
+
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import axes3d
+
+
+a = 1
+b = 1
+c = 0
+x = np.array([a, 0, 0])
+fx = np.poly1d(x)
+y = np.array([b, 0, 0])
+fy = np.poly1d(y)
+
+x0 = 1
+y0 = 2
+f = fx(x0)+fy(y0)+c
+
+l = 0.5
+n = 1000
+
+
+def newton_method(x0, y0, fx, fy, c, l, n):
+    dfx = fx.deriv()
+    ddfx = dfx.deriv()
+    dfy = fy.deriv()
+    ddfy = dfy.deriv()
+
+    for _ in range(n):
+        x0 = x0-l*(dfx(x0)/ddfx(x0))
+        y0 = y0-l*(dfy(y0)/ddfy(y0))
+
+    z = fx(x0)+fy(y0)+c
+    print(f"x_min: {x0}\ny_min: {y0}\nz_min: {z}")
+    return x0, y0, z
+
+
+a, b, c = newton_method(x0, y0, fx, fy, c, l, n)
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+x = np.linspace(-10, 10, 100)
+y = np.linspace(-10, 10, 100)
+X, Y = np.meshgrid(x, y)
+z = []
+for i in x:
+    t = []
+    for j in y:
+        t.append(fx(i)+fy(j)+c)
+    z.append(t)
+z = np.array(z)
+
+ax.plot_surface(X, Y, z, cmap='rainbow')
+
+plt.show()
